2bbd7f457b265385aac419a51f0e7150dd4efa76
edolubis21 <edolubis12345@gmail.com>
2022-04-10 23:41:27 +0700

    first commit

--- CardWarGame/ViewController.swift
+++ CardWarGame/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftSocre = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let (_,_) = randomCard()
     }
-
-
+    
+    func randomCard ()-> (Bool, Bool){
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        return (leftNumber > rightNumber , rightNumber > leftNumber)
+        
+    }
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let (left,right) = randomCard()
+        
+        if left {
+            leftSocre += 1
+        } else if right{
+            rightScore += 1
+        }
+        
+        rightScoreLabel.text = "\(rightScore)"
+        leftScoreLabel.text = "\(leftSocre)"
+        
+    }
+    
 }
 
